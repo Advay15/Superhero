bd1bbbff18f39482e48180b5de7fddad455743e0
Advay15 <79146419+Advay15@users.noreply.github.com>
2021-06-06 15:23:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ function player_update() {
         canvas.add(player_object);
     });
 }
+function new_image(get_image) {
+    fabric.Image.fromURL(get_image, function (Img) {
+        block_image_object = Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(block_image_object);
+    });
+}
 window.addEventListener("keydown",my_keyDown);
 function my_keyDown(e){
     keyPressed=e.keyCode;
